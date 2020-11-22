a77d7bad71a2c10b77c9b0a71f74add79825b424
BuddhikaD <buddhiextuse@gmail.com>
2020-11-22 23:17:08 +0530

    Event Handling

--- app/src/main/java/com/example/exercise/MainActivity.java
+++ app/src/main/java/com/example/exercise/MainActivity.java
@@ -2,66 +2,82 @@ package com.example.exercise;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.util.Log;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView text;
     private static final String TAG = "exercise";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG,"onCreate");
+        Log.i(TAG, "onCreate");
+
+        button = findViewById(R.id.btnOk);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = findViewById(R.id.txtHello);
+                text.setText("Hello Buddhika");
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG,"onStart");
+        Log.i(TAG, "onStart");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(TAG,"onRestart");
+        Log.i(TAG, "onRestart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG,"onPause");
+        Log.i(TAG, "onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG,"onResume");
+        Log.i(TAG, "onResume");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG,"onStop");
+        Log.i(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG,"onDestroy");
+        Log.i(TAG, "onDestroy");
     }
 
     @Override
-    protected void onRestoreInstanceState( Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.i(TAG,"onRestoreInstanceState");
+        Log.i(TAG, "onRestoreInstanceState");
     }
 
     @Override
-    protected void onSaveInstanceState( Bundle outState) {
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.i(TAG,"onSaveInstanceState");
+        Log.i(TAG, "onSaveInstanceState");
     }
 }
